3b1dcca2878b143907495b570e7e47c0fb251ff0
Lilit Grigoryan <lilit.grigoryan@accenture.com>
2019-11-29 18:14:46 +0200

    directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
 import { ProductsComponent } from './products/products.component';
+import { SearchBoxDirective } from './search-box.directive';
+import { SearchBox2Directive } from './search-box2/search-box2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
     OrdersComponent,
-    ProductsComponent
+    ProductsComponent,
+    SearchBoxDirective,
+    SearchBox2Directive
   ],
   imports: [
     BrowserModule
