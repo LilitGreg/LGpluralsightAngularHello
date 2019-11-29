39a11d52911c014d7e1b6343f21b9912c6f012ac
Lilit Grigoryan <lilit.grigoryan@accenture.com>
2019-11-29 17:55:57 +0200

    happy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
